775a885f0df7b8c8e0c3558e3039e124ee50f6ef
jjm <jaredjmyers@gmail.com>
2021-04-06 15:52:39 -0400

    deleted scrap code

--- wifi_survey_project.py
+++ wifi_survey_project.py
@@ -20,26 +20,3 @@ graph.generate_heatmap()
 
 #netscan.iperf_scan()
 #netscan.iperf_print()
-
-
-#Create data
-#x = np.random.randn(70000)
-#y = np.random.randn(70000)
-
-#Create heatmap
-#heatmap, xedges, yedges = np.histogram2d(x, y, bins=(128,128))
-#extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
-
-#Gimme colorbar
-#hm=plt.imshow(marks, cmap='Blues',interpolation="nearest")
-
-#Plot heatmap
-#plt.clf()
-#plt.title('Crappy Heatmap Example')
-#plt.ylabel('y')
-#plt.xlabel('x')
-#plt.imshow(heatmap, extent=extent)
-#plt.show()
-
-#plt.imshow(heatmap.T, extent=extent, origin='lower')
-#plt.show()
